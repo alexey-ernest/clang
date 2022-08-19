886bc22cc40ab9c571cbf585a436ffe3ae953b06
alexey-ernest <alexey.ernest@gmail.com>
2022-08-19 13:12:43 -0700

    Remove comments: ignore quoted strings

--- uncomment.c
+++ uncomment.c
@@ -4,33 +4,46 @@
 #define IN 		1
 #define OUT 	0
 
-/* remove comments from C code; single line and
-   multiline */
+/* remove comments from C code; 
+   - single line and multi line comments;
+   - ignore // inside multi line comments;
+   - ignore "" inside comments */
 int main(int argc, char const *argv[])
 {
 	int i, c, pc;
 	char buf[BUF_LEN+1];
-	int comment = OUT;
+	int singlecomment = OUT;
 	int multicomment = OUT;
+	int quotedstring = OUT;
+
+	char str[100] = "should ignore // inside quoted strings";
 
 	i = 0;
 	buf[BUF_LEN] = '\0';
 
 	while ((c = getchar()) != EOF) {
-		if (c == '/' && pc == '/') {
-			// comment started
-			comment = IN;
-			--i;
-		} else if (c == '\n' && comment == IN) {
-			// comment ended
-			comment = OUT;
-		} else if (c == '*' && pc == '/') {
-			// multi line comment started
-			multicomment = IN;
-			--i;
-		} else if (c == '/' && pc == '*' && multicomment == IN) {
-			multicomment = OUT;
-			continue;
+		if (c == '"' && !quotedstring && !singlecomment && !multicomment) {
+			quotedstring = IN;
+		} else if (c == '"') {
+			quotedstring = OUT;
+		}
+
+		if (quotedstring == OUT) {
+			if (c == '/' && pc == '/' && !multicomment) {
+				// comment started
+				singlecomment = IN;
+				--i;
+			} else if (c == '\n' && singlecomment == IN) {
+				// comment ended
+				singlecomment = OUT;
+			} else if (c == '*' && pc == '/') {
+				// multi line comment started
+				multicomment = IN;
+				--i;
+			} else if (c == '/' && pc == '*' && multicomment == IN) {
+				multicomment = OUT;
+				continue;
+			}
 		}
 
 		if (i >= BUF_LEN) { // flush the buf
@@ -38,7 +51,7 @@ int main(int argc, char const *argv[])
 			i = 0;
 		}
 
-		if (comment == OUT && multicomment == OUT) {
+		if (singlecomment == OUT && multicomment == OUT) {
 			buf[i] = c;
 			++i;
 		}
