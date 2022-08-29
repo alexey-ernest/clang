7fe2e34b8e21e7f417744619e1c9b2853aea1b06
alexey-ernest <alexey.ernest@gmail.com>
2022-08-28 17:11:25 -0700

    atof: convert character string to double

---
+++ atof.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define MAXLEN 32
+
+double atof(char s[]);
+
+int main(int argc, char const *argv[])
+{
+	char s[MAXLEN];
+	int i, c;
+	while((c = getchar()) != EOF && c != '\n') {
+		s[i++] = c;
+	}
+
+	printf("%.10f\n", atof(s));
+
+	return 0;
+}
+
+double atof(char s[]) {
+	double val, pow;
+	int i, sign;
+
+	// skip spaces
+	for (i = 0; s[i] == ' ' || s[i] == '\t' || s[i] == '\n'; ++i)
+		;
+
+	sign = 1;
+	if (s[i] == '-') {
+		sign = -1;
+		++i;
+	}
+
+	for (val = 0.0; s[i] >= '0' && s[i] <= '9'; ++i) {
+		val = val * 10.0 + (s[i] - '0');
+	}
+
+	if (s[i] == '.') {
+		++i;
+	}
+	
+	for (pow = 1.0; s[i] >= '0' && s[i] <= '9'; ++i) {
+		val = val * 10.0 + (s[i] - '0');
+		pow *= 10.0;
+	}
+
+	return sign * val / pow;
+}
